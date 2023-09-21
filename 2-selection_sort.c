7c5228ddde8ca96f35ea46bba923989b53f72072
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-21 20:07:16 +0000

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+* swap - Swap two values
+*
+* @p: First num
+* @q: Second num
+*/
+void swap(int *p, int *q)
+{
+	int tmp;
+
+	tmp = *p;
+	*p = *q;
+	*q = tmp;
+}
+/**
 * selection_sort - Implements the selection sort algorithm
 *		   which sorts the elements of an array
 *
@@ -9,22 +23,22 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, num_pos;
-	int tmp;
+	int *min_idx;
+	size_t i, j;
 
-	if (array == NULL)
+	if (!array || size < 2)
 		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
-		num_pos = i;
+		min_idx = array + i;
 		for (j = i + 1; j < size; j++)
+			min_idx = (array[j] < *min_idx) ? (array + j) : min_idx;
+
+		if ((array + i) != min_idx)
 		{
-			if (array[j] < array[num_pos])
-				num_pos = j;
+			swap(array + i, min_idx);
+			print_array(array, size);
 		}
-		tmp = array[i];
-		array[i] = array[num_pos];
-		array[num_pos] = tmp;
-		print_array(array, size);
 	}
 }
