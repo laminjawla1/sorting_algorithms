fa79c065b54c0a49bef97bcd4079d6dfaadf4830
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-22 14:37:22 +0000

    Bubble short

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,18 +24,23 @@ void swap(int *p, int *q)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	bool flag;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(array + j, array + j + 1);
 				print_array(array, size);
+				flag = true;
 			}
 		}
+		if (!flag)
+			return;
 	}
 }
